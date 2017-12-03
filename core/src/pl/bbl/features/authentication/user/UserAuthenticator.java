ebf83dd7caf7812decdb104460d242a3eaef71ab
Patrick <bleowy1@gmail.com>
2017-12-03 03:41:29 +0100

    Saving up unfinished work.
    time to sleep or smth.

--- core/src/pl/bbl/features/authentication/user/UserAuthenticator.java
+++ core/src/pl/bbl/features/authentication/user/UserAuthenticator.java
@@ -1,9 +1,18 @@
 package pl.bbl.features.authentication.user;
 
+import pl.bbl.database.DatabaseConnection;
 import pl.bbl.network.packet.BasicPacket;
+import pl.bbl.server.user.User;
 
 public class UserAuthenticator {
-    public static void startLoginProcess(BasicPacket basicPacket){
+    private static DatabaseConnection databaseConnection;
 
+    public UserAuthenticator(DatabaseConnection databaseConnection){
+        UserAuthenticator.databaseConnection = databaseConnection;
+    }
+
+    public static void startLoginProcess(BasicPacket basicPacket, User user){
+        String login = (String) basicPacket.getData("login");
+        String password = (String) basicPacket.getData("password");
     }
 }
--- core/src/pl/bbl/features/authentication/user/handler/UserAuthenticationHandler.java
+++ core/src/pl/bbl/features/authentication/user/handler/UserAuthenticationHandler.java
@@ -1,19 +1,34 @@
 package pl.bbl.features.authentication.user.handler;
 
+import io.netty.channel.ChannelHandlerContext;
+import pl.bbl.database.DatabaseConnection;
+import pl.bbl.features.authentication.user.UserAuthenticator;
 import pl.bbl.network.packet.BasicPacket;
 import pl.bbl.network.server.handlers.UserHandler;
 import pl.bbl.network.server.hive.UserHive;
+import pl.bbl.server.user.User;
+
 
 public class UserAuthenticationHandler extends UserHandler{
-    public UserAuthenticationHandler(UserHive userHive) {
+    private UserAuthenticator userAuthenticator;
+    private User user;
+
+    public UserAuthenticationHandler(UserHive userHive, DatabaseConnection databaseConnection) {
         super(userHive);
+        userAuthenticator = new UserAuthenticator(databaseConnection);
     }
 
     @Override
     protected void handlePacket(BasicPacket basicPacket) {
         switch (basicPacket.packetType){
             case "START_LOGIN":
-
+                UserAuthenticator.startLoginProcess(basicPacket, user);
         }
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        super.channelActive(ctx);
+        user = (User) userHive.getUserByContextHandler(ctx);
     }
 }
