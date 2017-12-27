ad5dd7670673bcf90b2d00afc6b33249415e1f15
Patrick <bleowy1@gmail.com>
2017-12-27 23:53:28 +0100

    Updated network framework.

--- core/src/pl/bbl/features/servers/game/information/packets/UserVerificationPacket.java
+++ core/src/pl/bbl/features/servers/game/information/packets/UserVerificationPacket.java
@@ -4,7 +4,7 @@ import pl.bbl.network.packet.Packet;
 
 public class UserVerificationPacket {
     public static Packet createPacket(String userId, boolean result){
-        Packet packet = new Packet("INFORMATION_EXCHANGE", "USER_VERIFICATION_RESULT");
+        Packet packet = new Packet("INFORMATION_PACKETS", "USER_VERIFICATION_RESULT");
         packet.addData("userId", userId);
         packet.addData("result", result);
         return packet;
